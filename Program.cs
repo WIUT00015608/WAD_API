71766cdd77044dcd465f4c831c5e716567bb8902
00015608 <th1nkerldm@gmail.com>
2025-06-02 23:15:23 +0500

    changing the program cs for getting access to angular app

--- Program.cs
+++ Program.cs
@@ -6,21 +6,29 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers()
-    .AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-    );
+    .AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200") // Angular dev server
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 IConfiguration conf = builder.Configuration;
 string? connStr = conf.GetConnectionString("DefaultConnection");
-
 connStr = connStr?.Replace("|DbDir|", builder.Environment.ContentRootPath);
-
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
@@ -39,7 +47,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// Comment out HTTPS redirection for development to avoid port issues
+// app.UseHttpsRedirection();
+
+// Use CORS policy - IMPORTANT: This must be before UseAuthorization and MapControllers
+app.UseCors("AllowAngularApp");
 
 app.MapControllers();
+
 app.Run();
